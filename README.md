05c2a3302dc6f131c14ca45edc5a4ecb792a9db0
Christian Paul Dehli <cpdehli@gmail.com>
2015-04-27 13:33:01 -0400

    First substantial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+This site has been coded using Jekyll. It can be viewed at: [alphastarsprogram.com](http://alphastarsprogram.com)
---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Uses jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
